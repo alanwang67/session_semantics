bfec7ddd3fdd6ea4b88a065e20dfab1ed3733847
Alan Wang <alanwang599@gmail.com>
2025-02-24 15:18:19 -0600

    udpated server file

--- server/server.go
+++ server/server.go
@@ -19,7 +19,7 @@ type Message struct {
 	MessageType uint64
 
 	C2S_Client_Id            uint64
-	C2S_Client_RequestNumber uint64
+	C2S_Server_Id            uint64
 	C2S_Client_OperationType uint64
 	C2S_Client_Data          uint64
 	C2S_Client_VersionVector []uint64
@@ -36,15 +36,18 @@ type Message struct {
 	S2C_Client_OperationType uint64
 	S2C_Client_Data          uint64
 	S2C_Client_VersionVector []uint64
-	S2C_Client_RequestNumber uint64
+	S2C_Server_Id            uint64
 	S2C_Client_Number        uint64
 }
 
 type RpcServer struct {
-	Id      uint64
-	Self    *protocol.Connection
-	Peers   []*protocol.Connection
-	Clients []*protocol.Connection
+	Id                uint64
+	Self              *protocol.Connection
+	Peers             []*protocol.Connection
+	Clients           []*protocol.Connection
+	SelfConnection    *rpc.Client
+	PeersConnection   []*rpc.Client
+	ClientsConnection []*rpc.Client
 
 	UnsatisfiedRequests    []Message
 	VectorClock            []uint64
@@ -52,7 +55,6 @@ type RpcServer struct {
 	MyOperations           []Operation
 	PendingOperations      []Operation
 	GossipAcknowledgements []uint64
-	SeenRequests           []uint64
 	mu                     sync.Mutex
 }
 
@@ -65,22 +67,24 @@ type Server struct {
 	MyOperations           []Operation
 	PendingOperations      []Operation
 	GossipAcknowledgements []uint64
-	SeenRequests           []uint64
 }
 
-func New(id uint64, self *protocol.Connection, peers []*protocol.Connection, clients []*protocol.Connection) *RpcServer {
+func New(id uint64, self *protocol.Connection, peers []*protocol.Connection, clients []*protocol.Connection, selfConnection *rpc.Client,
+	peersConnection []*rpc.Client, clientsConnection []*rpc.Client) *RpcServer {
 	server := &RpcServer{
 		Id:                     id,
 		Self:                   self,
 		Peers:                  peers,
 		Clients:                clients,
+		SelfConnection:         selfConnection,
+		PeersConnection:        peersConnection,
+		ClientsConnection:      clientsConnection,
 		UnsatisfiedRequests:    make([]Message, 0),
 		VectorClock:            make([]uint64, len(peers)),
 		OperationsPerformed:    make([]Operation, 0),
 		MyOperations:           make([]Operation, 0),
 		PendingOperations:      make([]Operation, 0),
 		GossipAcknowledgements: make([]uint64, len(peers)),
-		SeenRequests:           make([]uint64, len(clients)),
 	}
 
 	return server
@@ -100,7 +104,7 @@ func compareVersionVector(v1 []uint64, v2 []uint64) bool {
 	return output
 }
 
-func lexiographicCompare(v1 []uint64, v2 []uint64) bool {
+func lexicographicCompare(v1 []uint64, v2 []uint64) bool {
 	var output = false
 	var i = uint64(0)
 	var l = uint64(len(v1))
@@ -144,16 +148,16 @@ func oneOffVersionVector(v1 []uint64, v2 []uint64) bool {
 	for i < l {
 		if canApply && v1[i]+1 == v2[i] {
 			canApply = false
-			i++
-		} else if v1[i] < v2[i] {
-			output = false
-			i++
-		} else {
-			i++
+			i = i + 1
+			continue
 		}
+		if v1[i] < v2[i] {
+			output = false
+		}
+		i = i + 1
 	}
 
-	return output // !canApply
+	return output && !canApply
 }
 
 func equalSlices(s1 []uint64, s2 []uint64) bool {
@@ -181,15 +185,13 @@ func binarySearch(s []Operation, needle Operation) uint64 {
 	var j = uint64(len(s))
 	for i < j {
 		mid := i + (j-i)/2
-		if lexiographicCompare(needle.VersionVector, s[mid].VersionVector) {
+		if lexicographicCompare(needle.VersionVector, s[mid].VersionVector) {
 			i = mid + 1
 		} else {
 			j = mid
 		}
 	}
-	if i < uint64(len(s)) {
-		return i
-	}
+
 	return i
 }
 
@@ -205,26 +207,38 @@ func sortedInsert(s []Operation, value Operation) []Operation {
 }
 
 func mergeOperations(l1 []Operation, l2 []Operation) []Operation {
-	var output = append([]Operation{}, l1...)
+	if (len(l1) == 0) && (len(l2) == 0) {
+		return make([]Operation, 0)
+	}
+
+	var intermediate = append([]Operation{}, l1...)
 	var i = uint64(0)
 	var l = uint64(len(l2))
 
 	for i < l {
-		output = sortedInsert(output, l2[i])
+		intermediate = sortedInsert(intermediate, l2[i])
 		i++
 	}
 
 	var prev = uint64(1)
 	var curr = uint64(1)
-	for curr < uint64(len(output)) {
-		if !equalOperations(output[curr-1], output[curr]) {
-			output[prev] = output[curr]
+	for curr < uint64(len(intermediate)) {
+		if !equalOperations(intermediate[curr-1], intermediate[curr]) {
+			intermediate[prev] = intermediate[curr]
 			prev = prev + 1
 		}
 		curr = curr + 1
 	}
 
-	return output[:prev]
+	var output = make([]Operation, 0)
+
+	i = uint64(0)
+	l = prev
+	for i < prev {
+		output = append(output, intermediate[i])
+		i = i + 1
+	}
+	return output
 }
 
 func deleteAtIndexOperation(l []Operation, index uint64) []Operation {
@@ -252,7 +266,6 @@ func receiveGossip(server Server, request Message) Server {
 	}
 
 	server.PendingOperations = mergeOperations(server.PendingOperations, request.S2S_Gossip_Operations)
-	// latestVersionVector := append([]uint64(nil), server.VectorClock...)
 
 	var i = uint64(0)
 
@@ -261,7 +274,6 @@ func receiveGossip(server Server, request Message) Server {
 			server.OperationsPerformed = mergeOperations(server.OperationsPerformed, []Operation{server.PendingOperations[i]})
 			server.VectorClock = maxTS(server.VectorClock, server.PendingOperations[i].VersionVector)
 			server.PendingOperations = deleteAtIndexOperation(server.PendingOperations, i)
-			// latestVersionVector = append([]uint64(nil), server.VectorClock...)
 			continue
 		}
 		i = i + 1
@@ -271,45 +283,32 @@ func receiveGossip(server Server, request Message) Server {
 }
 
 func acknowledgeGossip(server Server, request Message) Server {
-	server.GossipAcknowledgements[request.S2S_Acknowledge_Gossip_Sending_ServerId] = request.S2S_Acknowledge_Gossip_Index - 1 // double check -1
+	server.GossipAcknowledgements[request.S2S_Acknowledge_Gossip_Sending_ServerId] = maxTwoInts(server.GossipAcknowledgements[request.S2S_Acknowledge_Gossip_Sending_ServerId], request.S2S_Acknowledge_Gossip_Index)
 	return server
 }
 
 func getGossipOperations(server Server, serverId uint64) []Operation {
-	// fmt.Println(len(server.MyOperations[server.GossipAcknowledgements[serverId]:]))
 	return append([]Operation(nil), server.MyOperations[server.GossipAcknowledgements[serverId]:]...)
 }
 
-func checkIfDuplicateRequest(server Server, request Message) bool {
-	return server.SeenRequests[request.C2S_Client_Id] >= request.C2S_Client_RequestNumber
-}
-
 func processClientRequest(server Server, request Message) (bool, Server, Message) {
-	fmt.Println(request.C2S_Client_RequestNumber)
-
 	reply := Message{}
 
-	// fmt.Print(request.C2S_Client_VersionVector)
-	// fmt.Print(compareVersionVector(server.VectorClock, request.C2S_Client_VersionVector))
-	// fmt.Print(checkIfDuplicateRequest(server, request))
-	if !equalSlices(make([]uint64, server.NumberOfServers), request.C2S_Client_VersionVector) && (!(compareVersionVector(server.VectorClock, request.C2S_Client_VersionVector)) || checkIfDuplicateRequest(server, request)) {
+	if !compareVersionVector(server.VectorClock, request.C2S_Client_VersionVector) {
 		return false, server, reply
 	}
 
 	if request.C2S_Client_OperationType == 0 {
-		server.SeenRequests[request.C2S_Client_Id] = request.C2S_Client_RequestNumber
-
 		reply.MessageType = 4
 		reply.S2C_Client_OperationType = 0
 		reply.S2C_Client_Data = getDataFromOperationLog(server.OperationsPerformed)
-		reply.S2C_Client_VersionVector = maxTS(request.C2S_Client_VersionVector, server.VectorClock)
+		reply.S2C_Client_VersionVector = server.VectorClock
+		reply.S2C_Server_Id = server.Id
 		reply.S2C_Client_Number = request.C2S_Client_Id
-		reply.S2C_Client_RequestNumber = request.C2S_Client_RequestNumber
 
 		return true, server, reply
 	} else {
 		server.VectorClock[server.Id] += 1
-		server.SeenRequests[request.C2S_Client_Id] = request.C2S_Client_RequestNumber
 
 		server.OperationsPerformed = append(server.OperationsPerformed, Operation{
 			VersionVector: append([]uint64(nil), server.VectorClock...),
@@ -323,12 +322,10 @@ func processClientRequest(server Server, request Message) (bool, Server, Message
 
 		reply.MessageType = 4
 		reply.S2C_Client_OperationType = 1
-		reply.S2C_Client_Data = getDataFromOperationLog(server.OperationsPerformed)
+		reply.S2C_Client_Data = 0
 		reply.S2C_Client_VersionVector = append([]uint64(nil), server.VectorClock...)
+		reply.S2C_Server_Id = server.Id
 		reply.S2C_Client_Number = request.C2S_Client_Id
-		reply.S2C_Client_RequestNumber = request.C2S_Client_RequestNumber
-
-		// fmt.Println(reply)
 
 		return true, server, reply
 	}
@@ -337,19 +334,16 @@ func processClientRequest(server Server, request Message) (bool, Server, Message
 func processRequest(server Server, request Message) (Server, []Message) {
 	var outGoingRequests = make([]Message, 0)
 	var s = server
-	if request.MessageType == 0 { // Regular client request
+	if request.MessageType == 0 {
 		var succeeded = false
 		var reply = Message{}
 		succeeded, s, reply = processClientRequest(s, request)
-		// fmt.Print(s.MyOperations)
 		if succeeded {
 			outGoingRequests = append(outGoingRequests, reply)
 		} else {
 			s.UnsatisfiedRequests = append(s.UnsatisfiedRequests, request)
 		}
-		// fmt.Print(s, outGoingRequests)
-	} else if request.MessageType == 1 { // receiving a gossip request
-		// fmt.Print(request.S2S_Gossip_Operations)
+	} else if request.MessageType == 1 {
 		s = receiveGossip(s, request)
 		outGoingRequests = append(outGoingRequests,
 			Message{MessageType: 2,
@@ -371,12 +365,9 @@ func processRequest(server Server, request Message) (Server, []Message) {
 			i++
 		}
 
-	} else if request.MessageType == 2 { // acknowledging a gossip request
-		// fmt.Print("WE GET")
+	} else if request.MessageType == 2 {
 		s = acknowledgeGossip(s, request)
-	} else if request.MessageType == 3 { // sending gossip request
-		// fmt.Print("We are gossiping")
-
+	} else if request.MessageType == 3 {
 		var i = uint64(0)
 		for i < server.NumberOfServers {
 			if uint64(i) != uint64(s.Id) {
@@ -386,14 +377,13 @@ func processRequest(server Server, request Message) (Server, []Message) {
 						S2S_Gossip_Sending_ServerId:   s.Id,
 						S2S_Gossip_Receiving_ServerId: index,
 						S2S_Gossip_Operations:         getGossipOperations(s, index),
-						S2S_Gossip_Index:              uint64(len(s.MyOperations)),
+						S2S_Gossip_Index:              uint64(len(s.MyOperations) - 1),
 					})
 			}
 			i = i + 1
 		}
 	}
 
-	// fmt.Print(s.MyOperations)
 	return s, outGoingRequests
 }
 
@@ -409,7 +399,6 @@ func (s *RpcServer) RpcHandler(request *Message, reply *Message) error {
 			MyOperations:           s.MyOperations,
 			PendingOperations:      s.PendingOperations,
 			GossipAcknowledgements: s.GossipAcknowledgements,
-			SeenRequests:           s.SeenRequests,
 		}, *request)
 
 	s.UnsatisfiedRequests = ns.UnsatisfiedRequests
@@ -418,7 +407,6 @@ func (s *RpcServer) RpcHandler(request *Message, reply *Message) error {
 	s.MyOperations = ns.MyOperations
 	s.PendingOperations = ns.PendingOperations
 	s.GossipAcknowledgements = ns.GossipAcknowledgements
-	s.SeenRequests = ns.SeenRequests
 
 	s.mu.Unlock()
 
@@ -474,7 +462,7 @@ func (s *RpcServer) Start() error {
 
 	go func() error {
 		for {
-			ms := 100
+			ms := 50
 
 			time.Sleep(time.Duration(ms) * time.Millisecond)
 
